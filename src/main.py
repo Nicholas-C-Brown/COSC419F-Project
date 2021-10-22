2b07e75b1f1f8a8c048c091566382748bf68b341
Nicholas-C-Brown <nicholas.c.brown@alumni.ubc.ca>
2021-10-22 00:58:46 -0700

    linkedin profile url

--- src/main.py
+++ src/main.py
@@ -9,14 +9,18 @@ import validators
 driver_path = 'COSC419F-Project/assets/driver/chromedriver.exe'
 is_headless = False
 
+linkedin_profile_baseurl = "https://www.linkedin.com/in/"
+
 # APPLICATION START
 
 # Get url from user
-url = input("Enter a URL: ")
+print("Enter your LinkedIn profile URL.")
+url = linkedin_profile_baseurl + input(linkedin_profile_baseurl)
 
 if(not validators.url(url)):
-    print("\nPlease enter a valid url.\nExample: 'https://google.com'")
+    print("\nPlease enter a valid url.\nExample: https://www.linkedin.com/in/nicholas-c-brown")
     exit(1)
+
 
 # Start Chrome Webdriver
 driver = driverHelper.configure_driver(driver_path, is_headless)
