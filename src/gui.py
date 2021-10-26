89175bd7f05b517e89ac16f9de06dc33d7bb8b9a
Ross <ross.morrison312@gmail.com>
2021-10-25 20:01:16 -0700

    change(gui): Add cancel button

--- src/gui.py
+++ src/gui.py
@@ -13,9 +13,20 @@ def prompt_for_url(base_url):
     name_label.pack()
     name_entry = tk.Entry(window, width=70, textvariable=name)
     name_entry.pack()
-    submit_button = tk.Button(window, text="Submit", command=window.destroy)
-    submit_button.pack()
+    # Add a buttongroup with 2 buttons
+    button_frame = tk.Frame(window)
+    button_frame.pack()
+    submit_button = tk.Button(
+        button_frame, text="Submit", command=window.destroy)
+    submit_button.pack(side=tk.LEFT)
+    # Add a cancel button that will exit(1) when clicked
+    cancel_button = tk.Button(
+        button_frame, text="Cancel", command=lambda: exit(1))
+    cancel_button.pack(side=tk.RIGHT)
     window.mainloop()
+    # On window close, exit the program
+    if name.get() == base_url:
+        exit(1)
     return name.get()
 
 # Write a function that shows an error message
