58addc48f5a0441b47204bc2467a383382bf8c5f
Ross <ross.morrison312@gmail.com>
2021-10-25 20:12:43 -0700

    Feature: GUI (#8)
    * feat(gui): Basic GUI implementation
    
    * feat(gui): Redesign and error cleanup
    
    * chore(gui): formatting
    
    * change(gui): Add cancel button

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from helper_methods.driver_helper import configure_driver as configure
 from helper_methods.linkedin_helper import linkedin_profile_baseurl as profile_baseurl, linkedin_exampleurl as example_url, \
     login, validate_profile_url
 from models.user_profile import get_user_profile
+import gui
 
 driver_path = 'assets/driver/chromedriver.exe'
 is_headless = False
@@ -9,12 +10,13 @@ is_headless = False
 # APPLICATION START
 
 # Get url from user
-print("Enter your LinkedIn profile URL.")
-url = profile_baseurl + input(profile_baseurl)
-
-if not validate_profile_url(url):
-    print("\nPlease enter a valid url.\nExample: " + example_url)
-    exit(1)
+while True:
+    url = gui.prompt_for_url(linkedin_profile_baseurl)
+    if validators.url(url):
+        break
+    else:
+        gui.show_error_message(
+            "Please enter a valid url.\nExample: https://www.linkedin.com/in/nicholas-c-brown")
 
 # Start Chrome Webdriver and Login
 driver = configure(driver_path, is_headless)
@@ -23,4 +25,3 @@ login(driver)
 user_profile = get_user_profile(url, driver)
 
 user_profile.print_profile()
-
